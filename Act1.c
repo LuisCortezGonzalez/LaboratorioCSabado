f49726bae8ed22fa0b43eb8f364536ea5adb6352
Luis Cortez <luis_gonzalez.95@hotmail.com>
2019-08-25 18:32:18 -0500

    Actividad 1

--- Act1.c
+++ Act1.c
@@ -3,7 +3,12 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	// Este comentario es para identificar el branch Practica2
+int main() {
+	float x;
+	float r;
+	printf ("Inserta una variable: ");
+  	scanf("%f",&x);
+	r = (((x-1.0)/4.0) - ((x-5.0)/36.0));
+	printf("%.6f", r);
 	return 0;
 }
