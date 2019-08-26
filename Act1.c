8cd708bb93ac68d9f780de07ce4d9eb962ed3eda
Luis Cortez <luis_gonzalez.95@hotmail.com>
2019-08-25 20:02:28 -0500

    Actividad 4

--- Act1.c
+++ Act1.c
@@ -11,16 +11,41 @@
 int main() {
 	float x;
 	float r1,r2,r3,r4;
+	/* se introducira una variable*/
 	printf ("Inserta una variable: ");
   	scanf("%f",&x);
+  	
+  	/* se hace la primera ecuacion*/
+  	
 	r1 = (((x-1.0)/4.0) - ((x-5.0)/36.0));
+	
+	/* se imprimira el resultado */
+	
 	printf("\n\n\t\t Respuesta 1: %.6f", r1);
 	
+	/* se hace la segunda ecuacion*/
+	
 	r2 = 6.0*(((x+1.0)/8.0) - ((2.0*x-3.0)/16.0));
+	
+	/* se imprimira el resultado */
+	
 	printf("\n\n\t\t Respuesta 2: %.6f", r2);
 	
+	/* se hace la tecera ecuacion*/
+	
 	r3 = ((2.0/3.0)*(x-(1.0-(x-2.0)/3.0)) + 1.0);
+	
+	/* se imprimira el resultado */
+	
 	printf("\n\n\t\t Respuesta 3: %.6f", r3);
+	
+	/* se hace la cuarta ecuacion*/
+	
+	r4 = 2-(-2*(x+1)-((x-3)/2));
+	
+	/* se imprimira el resultado */
+	
+	printf("\n\n\t\t Respuesta 4: %.6f", r4);
 	
 	return 0;
 }
