9015cbfe063eed405aa49f68396dd76382766c82
Luis Cortez <luis_gonzalez.95@hotmail.com>
2019-08-25 19:50:08 -0500

    Actividad 3

--- Act1.c
+++ Act1.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* *Autor: Luis Eduardo Cortez Gonzalez
+   *Fecha: 25/08/2019
+   *Grupo: 
+   *Horario: Sabado 9:00am - 11:00am
+   *Matricula: 1546624
+   */
 
 int main() {
 	float x;
@@ -11,8 +16,11 @@ int main() {
 	r1 = (((x-1.0)/4.0) - ((x-5.0)/36.0));
 	printf("\n\n\t\t Respuesta 1: %.6f", r1);
 	
-	r2 = 6*(((x+1.0)/8.0) - ((2*x-3.0)/16.0));
+	r2 = 6.0*(((x+1.0)/8.0) - ((2.0*x-3.0)/16.0));
 	printf("\n\n\t\t Respuesta 2: %.6f", r2);
+	
+	r3 = ((2.0/3.0)*(x-(1.0-(x-2.0)/3.0)) + 1.0);
+	printf("\n\n\t\t Respuesta 3: %.6f", r3);
 	
 	return 0;
 }
