50ed2b5be2f4c233f0a7c55bf71a27e2474ec83b
Luis Cortez <luis_gonzalez.95@hotmail.com>
2019-10-12 22:39:28 -0500

    Practica10

--- main.c
+++ main.c
@@ -1,9 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* Autor: Luis Eduardo Cortez Gonzalez
+   Fecha: 12/10/2019
+   Grupo: 008
+   Horario: Sabado 9am-11am
+   Matricula: 1546624 
+ */
 
-int main(int argc, char *argv[]) {
+int main() {
+	
+	//xxxxx ACTIVIDAD 1 xxxxx
+	
+	//se declaran las variables
+	char texto[' '];
+    int i, n;
+    int pali = 1;
+    
+    //Se pregunta cual es la cadena a ingresar
+    printf("\n Ingrese la cadena de texto: ");
+    fflush(stdin);
+    gets(texto);
+    //se cuenta cuantos caracteres ingreso
+    n=strlen(texto)-1;
+    
+    //para saber si es o no un palindromo
+    for(i=0; i<n; i++, n--) {
+        
+        if (*(texto+i) != *(texto+n)) {
+            pali = 0;
+            break;
+        }
+    }
+    if (pali)
+        printf("\n Es un palindromo \n");
+    else
+        printf("\n No es un palindromo \n");
+        
+        
+   //xxxxx ACTIVIDAD 2 xxxxx
+   
+   //se declaran las variables
+   char texto1[' '];
+   char alfa[26]={"abcdefghijklmnopqrstuvwxyz"};
+	int cont[26]={0};
+   int j=0,l,k;
+   
+   //se ingresa la cadena
+   printf("\n Ingrese la cadena de caracteres: ");
+  
+   gets(texto1);
+   
+   j=strlen(texto1);
+   //se va a hacer un contador para ver cuantas letras se repiten y se guarden en una variable
+   for(l=0;l<strlen(texto1);l++){
+   	
+   	for(k=0;k<26;k++){
+   		if(tolower(texto1[l])==alfa[k]){
+		   
+   		
+   		cont[k]++;
+   		
+	   }
+   }
+}
+	// se imprima las veces que se repite una letra
+	for(l=0;l<26;l++){
+		
+			
+	printf("\n las letras %c se repiten: %d",alfa[l],cont[l]);
+}
 	return 0;
 	//cambio
 }
